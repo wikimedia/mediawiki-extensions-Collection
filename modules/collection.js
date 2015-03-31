239b2599f30d723d878307365b62ffa57e9521d1
C. Scott Ananian <cscott@cscott.net>
2015-03-31 16:46:26 -0400

    Javascript whitespace and code style cleanup
    Change-Id: I421e0f15c6d28cd9bebcfcd4784b6390c8cfe0f8

--- modules/collection.js
+++ modules/collection.js
@@ -20,7 +20,7 @@
  */
 
 /*global confirm, prompt, collection_id, writer, wfCollectionSave */
-(function($) {
+( function ( $ ) {
 
 /******************************************************************************/
 
@@ -28,190 +28,195 @@
  * Return text of element with given selector. Optionally replace %PARAM% with value
  * of param. This allows usage of localization features in PHP from JavaScript.
  *
- * @param String id elment ID of elment containing text
- * @param String param optionally, a text to replace %PARAM% with
- * @return String text of elment with ID id
+ * @param {string} id Element ID of element containing text
+ * @param {string} [param] Text to replace %PARAM% with
+ * @return {string} text of elment with ID id
  */
-function gettext(sel, param/*=null*/) {
-	var txt = $(sel).html();
-	if (param) {
-		txt = txt.replace(/%PARAM%/g, param);
+function gettext( sel, param ) {
+	var txt = $( sel ).html();
+	if ( param ) {
+		txt = txt.replace( /%PARAM%/g, param );
 	}
 	return txt;
 }
 
-function req(func, args, callback) {
-	$.post(script_url, {
+function req( func, args, callback ) {
+	$.post( script_url, {
 		'action': 'ajax',
 		'rs': 'wfAjaxCollection' + func,
 		'rsargs[]': args
-	}, callback, 'json');
+	}, callback, 'json' );
 }
 
-var script_url = mw.util.wikiScript();
-
-var media_path = mw.config.get('wgScriptPath') + '/extensions/Collection/images/';
-
-var collapseicon = media_path + '/collapse.png';
-var expandicon   = media_path + '/expand.png';
-
-var chapter_max_len = 200;
-
-/******************************************************************************/
+var script_url = mw.util.wikiScript(),
+	media_path = mw.config.get( 'wgExtensionAssetsPath' ) + '/Collection/images/',
+	collapseicon = media_path + '/collapse.png',
+	expandicon = media_path + '/expand.png',
+	chapter_max_len = 200;
 
 function getMWServeStatus() {
-	$.getJSON(script_url, {
+	$.getJSON( script_url, {
 		'action': 'ajax',
 		'rs': 'wfAjaxGetMWServeStatus',
-		'rsargs[]': [collection_id, writer]
-	}, function(result) {
-		if (result.state === 'progress' ) {
-			if ( result.status.progress )  {
-				$('#renderingProgress').html( mw.language.convertNumber(
-					result.status.progress ) );
+		'rsargs[]': [ collection_id, writer ]
+	}, function ( result ) {
+		if ( result.state === 'progress' ) {
+			if ( result.status.progress ) {
+				$( '#renderingProgress' ).html(
+					mw.language.convertNumber( result.status.progress )
+				);
 			}
-			if (result.status.status) {
+			if ( result.status.status ) {
 				var status = result.status.status;
-				if (result.status.article) {
-					status += gettext('#renderingArticle', result.status.article);
-				} else if (result.status.page) {
-					status += gettext('#renderingPage', result.status.page);
+				if ( result.status.article ) {
+					status += gettext( '#renderingArticle', result.status.article );
+				} else if ( result.status.page ) {
+					status += gettext( '#renderingPage', result.status.page );
 				}
-				$('#renderingStatus').html(gettext('#renderingStatusText', status));
+				$( '#renderingStatus' ).html(
+					gettext( '#renderingStatusText', status )
+				);
 			}
-			setTimeout(getMWServeStatus, 500);
+			setTimeout( getMWServeStatus, 500 );
 		} else {
-			window.location.reload(true);
+			location.reload( true );
 		}
-	});
+	} );
 }
 
-/******************************************************************************/
-
 function clear_collection() {
-	if (confirm(gettext('#clearCollectionConfirmText'))) {
-		req('Clear',
+	if ( confirm( gettext( '#clearCollectionConfirmText' ) ) ) {
+		req( 'Clear',
 			[],
-			function(result) {
-				$('#titleInput').val('');
-				$('#subtitleInput').val('');
-				refresh_list(result);
-				req('GetBookCreatorBoxContent', ['showbook', null, mw.config.get('wgPageName')], function(result2) {
-					$('#coll-book_creator_box').html(result2.html);
-				});
-			});
+			function ( result ) {
+				$( '#titleInput, #subtitleInput' ).val( '' );
+				refresh_list( result );
+				req( 'GetBookCreatorBoxContent', [
+					'showbook',
+					null,
+					mw.config.get( 'wgPageName' )
+				], function ( result2 ) {
+					$( '#coll-book_creator_box' ).html( result2.html );
+				} );
+			} );
 	}
 	return false;
 }
 
 function create_chapter() {
-	var name = prompt(gettext('#newChapterText'));
-	if (name) {
-		name = name.substring(0, chapter_max_len);
-		req('AddChapter', [name], refresh_list);
+	var name = prompt( gettext( '#newChapterText' ) );
+	if ( name ) {
+		name = name.substring( 0, chapter_max_len );
+		req( 'AddChapter', [ name ], refresh_list );
 		update_buttons();
 	}
 	return false;
 }
 
-function rename_chapter(index, old_name) {
-	var new_name = prompt(gettext('#renameChapterText'), old_name);
-	if (new_name) {
-		new_name = new_name.substring(0, chapter_max_len);
-		req('RenameChapter', [index, new_name], refresh_list);
+function rename_chapter( index, old_name ) {
+	var new_name = prompt( gettext( '#renameChapterText' ), old_name );
+	if ( new_name ) {
+		new_name = new_name.substring( 0, chapter_max_len );
+		req( 'RenameChapter', [ index, new_name ], refresh_list );
 	}
 	return false;
 }
 
-function remove_item(index) {
-	req('RemoveItem',
-		[index],
-		function(result) {
-			refresh_list(result);
-			req('GetBookCreatorBoxContent', ['showbook', null, mw.config.get('wgPageName')], function(result2) {
-				$('#coll-book_creator_box').html(result2.html);
-			});
-		});
+function remove_item( index ) {
+	req( 'RemoveItem',
+		[ index ],
+		function ( result ) {
+			refresh_list( result );
+			req( 'GetBookCreatorBoxContent', [
+				'showbook',
+				null,
+				mw.config.get( 'wgPageName' )
+			], function ( result2 ) {
+				$( '#coll-book_creator_box' ).html( result2.html );
+			} );
+		} );
 	return false;
 }
 
 function set_titles() {
 	var settings = {};
 	$( '[id^="coll-input-setting-"]' ).each( function ( i, e ) {
-		if ( $( e ).is(':checkbox') ) {
-			settings[e.name] = $( e ).is(':checked');
+		if ( $( e ).is( ':checkbox' ) ) {
+			settings[e.name] = $( e ).is( ':checked' );
 		} else {
 			settings[e.name] = $( e ).val();
 		}
 	} );
-	req('SetTitles', [$('#titleInput').val(), $('#subtitleInput').val(), JSON.stringify( settings )], function(result) {
-		wfCollectionSave(result.collection);
-	});
+	req( 'SetTitles', [
+		$( '#titleInput' ).val(),
+		$( '#subtitleInput' ).val(),
+		JSON.stringify( settings )
+	], function ( result ) {
+		wfCollectionSave( result.collection );
+	} );
 	update_buttons();
 	return false;
 }
 
-function set_sorting(items_string) {
-	req('SetSorting', [items_string], refresh_list);
+function set_sorting( items_string ) {
+	req( 'SetSorting', [ items_string ], refresh_list );
 	return false;
 }
 
 function update_buttons() {
-	if ($('#collectionList .article').length === 0) {
-		$('#saveButton').attr('disabled', 'disabled');
-		$('#downloadButton').attr('disabled', 'disabled');
-		$('input.order').attr('disabled', 'disabled');
+	if ( $( '#collectionList .article' ).length === 0 ) {
+		$( '#saveButton, #downloadButton, input.order' ).attr( 'disabled', 'disabled' );
 		return;
 	} else {
-		$('#downloadButton').removeAttr('disabled');
-		$('input.order').removeAttr('disabled');
+		$( '#downloadButton, input.order' ).removeAttr( 'disabled' );
 	}
-	if (!$('#saveButton').length) {
+	if ( !$( '#saveButton' ).length ) {
 		return;
 	}
-	if (!$('#communityCollTitle').length || $('#personalCollType:checked').val()) {
-		$('#personalCollTitle').removeAttr('disabled');
-		$('#communityCollTitle').attr('disabled', 'disabled');
-		if (!$.trim($('#personalCollTitle').val())) {
-			$('#saveButton').attr('disabled', 'disabled');
+	if ( !$( '#communityCollTitle' ).length || $( '#personalCollType:checked' ).val() ) {
+		$( '#personalCollTitle' ).removeAttr( 'disabled' );
+		$( '#communityCollTitle' ).attr( 'disabled', 'disabled' );
+		if ( !$.trim( $( '#personalCollTitle' ).val() ) ) {
+			$( '#saveButton' ).attr( 'disabled', 'disabled' );
 			return;
 		}
-	} else if (!$('#personalCollTitle').length || $('#communityCollType:checked').val()) {
-		$('#communityCollTitle').removeAttr('disabled');
-		$('#personalCollTitle').attr('disabled', 'disabled');
-		if (!$.trim($('#communityCollTitle').val())) {
-			$('#saveButton').attr('disabled', 'disabled');
+	} else if ( !$( '#personalCollTitle' ).length || $( '#communityCollType:checked' ).val() ) {
+		$( '#communityCollTitle' ).removeAttr( 'disabled' );
+		$( '#personalCollTitle' ).attr( 'disabled', 'disabled' );
+		if ( !$.trim( $( '#communityCollTitle' ).val() ) ) {
+			$( '#saveButton' ).attr( 'disabled', 'disabled' );
 			return;
 		}
 	}
-	$('#saveButton').removeAttr('disabled');
+	$( '#saveButton' ).removeAttr( 'disabled' );
 }
 
 function make_sortable() {
-	$('#collectionList').sortable({
+	$( '#collectionList' ).sortable( {
 		axis: 'y',
-		update: function() {
-			set_sorting($('#collectionList').sortable('serialize'));
+		update: function () {
+			set_sorting( $( '#collectionList' ).sortable( 'serialize' ) );
 		}
-	});
-	$('#collectionList .sortableitem').css('cursor', 'move');
+	} );
+	$( '#collectionList .sortableitem' ).css( 'cursor', 'move' );
 }
 
-function refresh_list(data) {
-	wfCollectionSave(data.collection);
-	$('#collectionListContainer').html(data.html);
-	$('.makeVisible').css('display', 'inline');
+function refresh_list( data ) {
+	wfCollectionSave( data.collection );
+	$( '#collectionListContainer' ).html( data.html );
+	$( '.makeVisible' ).css( 'display', 'inline' );
 	make_sortable();
 	update_buttons();
 }
-function sort_items(){
-	req('SortItems', [], refresh_list);
+
+function sort_items() {
+	req( 'SortItems', [], refresh_list );
 	return false;
 }
 
-$(function() {
-	if ($('#collectionList').length) {
-		$('.makeVisible').css('display', 'inline');
+$( function () {
+	if ( $( '#collectionList' ).length ) {
+		$( '.makeVisible' ).css( 'display', 'inline' );
 		window.coll_create_chapter = create_chapter;
 		window.coll_remove_item = remove_item;
 		window.coll_rename_chapter = rename_chapter;
@@ -219,36 +224,38 @@ $(function() {
 		window.coll_sort_items = sort_items;
 		update_buttons();
 		make_sortable();
-		$('#coll-orderbox li.collection-partner.coll-more_info.collapsed').css(
-				'list-style' , 'url("' + collapseicon + '")');
-		$('#coll-orderbox').on('click',
-					'li.collection-partner.coll-more_info a.coll-partnerlink', function (event) {
+		$( '#coll-orderbox li.collection-partner.coll-more_info.collapsed' ).css(
+			'list-style',
+			'url("' + collapseicon + '")'
+		);
+		$( '#coll-orderbox' ).on(
+			'click',
+			'li.collection-partner.coll-more_info a.coll-partnerlink',
+			function ( event ) {
 			event.preventDefault();
 			event.stopPropagation();
-			var p = $(this).parents('li.collection-partner');
-			if (p.hasClass("collapsed")) {
-				p.css('list-style',  'url("' + expandicon + '")');
-				p.find('.coll-order_info').css('display', 'block');
-				p.removeClass("collapsed");
+			var p = $( this ).parents( 'li.collection-partner' );
+			if ( p.hasClass( 'collapsed' ) ) {
+				p.css( 'list-style',  'url("' + expandicon + '")' );
+				p.find( '.coll-order_info' ).css( 'display', 'block' );
+				p.removeClass( 'collapsed' );
 			} else {
-				p.css('list-style' , 'url("' + collapseicon + '")');
-				p.find('.coll-order_info').css('display', 'none');
-				p.addClass("collapsed");
+				p.css( 'list-style', 'url("' + collapseicon + '")' );
+				p.find( '.coll-order_info' ).css( 'display', 'none' );
+				p.addClass( 'collapsed' );
 			}
-		});
-		$('#personalCollTitle').val($('#titleInput').val());
-		$('#personalCollTitle').change(update_buttons);
-		$('#communityCollTitle').val($('#titleInput').val());
-		$('#communityCollTitle').change(update_buttons);
-		$('#personalCollType').change(update_buttons);
-		$('#communityCollType').change(update_buttons);
-		$('#titleInput').change(set_titles);
-		$('#subtitleInput').change(set_titles);
-		$( '[id^="coll-input-setting-"]' ).change(set_titles);
+		} );
+		$( '#personalCollTitle, #communityCollTitle' )
+			.val( $( '#titleInput' ).val() )
+			.change( update_buttons );
+		$( '#personalCollType, #communityCollType' )
+			.change( update_buttons );
+		$( '#titleInput, #subtitleInput, [id^="coll-input-setting-"]' )
+			.change( set_titles );
 	}
-	if (typeof collection_rendering !== 'undefined') {
+	if ( typeof collection_rendering !== 'undefined' ) {
 		getMWServeStatus();
 	}
-});
+} );
 
-})(jQuery);
+} )( jQuery );
--- modules/suggest.js
+++ modules/suggest.js
@@ -1,43 +1,47 @@
 /*global wfCollectionSave */
-(function($) {
+( function ( $ ) {
 
 var script_url = mw.util.wikiScript();
 
-function set_status(html) {
-	if (html) {
-		$('#collectionSuggestStatus').css('visibility', 'visible').html(html);
+function set_status( html ) {
+	if ( html ) {
+		$( '#collectionSuggestStatus' )
+			.css( 'visibility', 'visible' )
+			.html( html );
 	} else {
-		$('#collectionSuggestStatus').css('visibility', 'hidden').html('&nbsp;');
+		$( '#collectionSuggestStatus' )
+			.css( 'visibility', 'hidden' )
+			.html( '&nbsp;' );
 	}
 }
 
-function collectionSuggestCall(func, args) {
-	set_status('...');
-	$.post(script_url, {
+function collectionSuggestCall( func, args ) {
+	set_status( '...' );
+	$.post( script_url, {
 		'action': 'ajax',
 		'rs': 'wfAjaxCollectionSuggest' + func,
 		'rsargs[]': args
-	}, function(result) {
-		wfCollectionSave(result.collection);
-		if (func === 'undo') {
-			set_status(false);
+	}, function ( result ) {
+		wfCollectionSave( result.collection );
+		if ( func === 'undo' ) {
+			set_status( false );
 		} else {
-			set_status(result.last_action);
+			set_status( result.last_action );
 		}
-		$('#collectionSuggestions').html(result.suggestions_html);
-		$('#collectionMembers').html(result.members_html);
-		$('#coll-num_pages').text(result.num_pages);
-		$.getJSON(script_url, {
+		$( '#collectionSuggestions' ).html( result.suggestions_html );
+		$( '#collectionMembers' ).html( result.members_html );
+		$( '#coll-num_pages' ).text( result.num_pages );
+		$.getJSON( script_url, {
 			'action': 'ajax',
 			'rs': 'wfAjaxCollectionGetBookCreatorBoxContent',
-			'rsargs[]': ['suggest', null, mw.config.get('wgPageName')]
-		}, function(result) {
-			$('#coll-book_creator_box').html(result.html);
-		});
-	}, 'json');
+			'rsargs[]': [ 'suggest', null, mw.config.get( 'wgPageName' ) ]
+		}, function ( result ) {
+			$( '#coll-book_creator_box' ).html( result.html );
+		} );
+	}, 'json' );
 }
 
 window.collectionSuggestCall = collectionSuggestCall;
 
-})(jQuery);
+} )( jQuery );
 
