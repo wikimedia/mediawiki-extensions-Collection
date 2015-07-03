wikimedia/mediawiki-extensions-Collection

commit e126f75b73a86436c5728b4cc3484ed50f9cc128

Use VRS RESTBase/Parsoid configuration, if present.
Rather than falling back to the VisualEditor configuration globals,
use the VRS configuration for RESTBase or Parsoid.

This logic actually mirrors the logic used by VisualEditor, which also
prefers to use a VRS configuration if present.

Change-Id: I3c8e2060ce891d317d06eec120b7a254c133aa8b

RenderingAPI.php
@@ -213,14 +213,48 @@ abstract class CollectionRenderingAPI {
 			),
 		);
 
-		if ( class_exists( 'VisualEditorHooks' ) ) {
+		// Prefer VRS configuration if present.
+		$context = RequestContext::getMain();
+		$vrs = $context->getConfig()->get( 'VirtualRestConfig' );
+		if ( isset( $vrs['modules'] ) && isset( $vrs['modules']['restbase'] ) ) {
+			// if restbase is available, use it
+			$params = $vrs['modules']['restbase'];
+			$domain = preg_replace(
+				'/^(https?:\/\/)?([^\/:]+?)(\/|:\d+\/?)?$/',
+				'$2',
+				$params['domain']
+			);
+			$url = preg_replace(
+				'#/?$#',
+				'/' + $domain + '/v1/',
+				$params['url']
+			);
+			for ( $i = 0; $i < count( $result['wikis'] ); $i++ ) {
+				$result['wikis'][$i]['restbase1'] = $url;
+			}
+		} elseif ( isset( $vrs['modules'] ) && isset( $vrs['modules']['parsoid'] ) ) {
+			// there's a global parsoid config, use it next
+			$params = $vrs['modules']['parsoid'];
+			$domain = preg_replace(
+				'/^(https?:\/\/)?([^\/:]+?)(\/|:\d+\/?)?$/',
+				'$2',
+				$params['domain']
+			);
+			for ( $i = 0; $i < count( $result['wikis'] ); $i++ ) {
+				$result['wikis'][$i]['parsoid'] = $params['url'];
+				$result['wikis'][$i]['prefix'] = $params['prefix'];
+				$result['wikis'][$i]['domain'] = $domain;
+			}
+		} elseif ( class_exists( 'VisualEditorHooks' ) ) {
+			// fall back to Visual Editor configuration globals
 			global $wgVisualEditorParsoidURL, $wgVisualEditorParsoidPrefix,
-				$wgVisualEditorRestbaseURL;
+				$wgVisualEditorParsoidDomain, $wgVisualEditorRestbaseURL;
 			for ( $i = 0; $i < count( $result['wikis'] ); $i++ ) {
 				// Parsoid connection information
 				if ($wgVisualEditorParsoidURL) {
 					$result['wikis'][$i]['parsoid'] = $wgVisualEditorParsoidURL;
 					$result['wikis'][$i]['prefix'] = $wgVisualEditorParsoidPrefix;
+					$result['wikis'][$i]['domain'] = $wgVisualEditorParsoidDomain;
 				}
 				// RESTbase connection information
 				if ($wgVisualEditorRestbaseURL) {

commit c91f39da3ab0aac37c5b1a89809802024173be60

Fix typo w/ VRS URL construction, commit e126f75
PHP's concatenation operator is ".", not "+". This creates invalid URLs
such as "http://10.2.2.17:72310" and thus failing jobs.

This fixes a regression introduced with
e126f75b73a86436c5728b4cc3484ed50f9cc128.

Change-Id: If39b50013cc3aed7a59e40e22eae0ac072e519c2

RenderingAPI.php
@@ -226,7 +226,7 @@ abstract class CollectionRenderingAPI {
 			);
 			$url = preg_replace(
 				'#/?$#',
-				'/' + $domain + '/v1/',
+				'/' . $domain . '/v1/',
 				$params['url']
 			);
 			for ( $i = 0; $i < count( $result['wikis'] ); $i++ ) {