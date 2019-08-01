8a83e17b2c0a14a2f4d49e5509f6ed5a9befd2d4
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2019-08-01 22:40:21 +0000

    build: Updating lodash to 4.17.15
    * https://npmjs.com/advisories/1065
    * CVE-2019-10744
    
    Additional changes:
    * Enable eslint caching.
    * Enable eslint's reportUnusedDisableDirectives.
    
    Change-Id: I6344b79d231852bdf64c2e1cadd2ce9a89f4245a

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,10 @@ module.exports = function ( grunt ) {
 
 	grunt.initConfig( {
 		eslint: {
+			options: {
+				cache: true,
+				reportUnusedDisableDirectives: true
+			},
 			all: [
 				'*.js',
 				'resources/**/*.js'
