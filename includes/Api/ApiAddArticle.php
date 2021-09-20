4354ab3edd8e77c882d1603936b8a4bc96f8c027
Derick Alangi <alangiderick@gmail.com>
2021-09-20 19:22:50 +0100

    Api: Introduce module to add an article to a user's collection
    Bug: T291399
    Change-Id: Id842cb70a9268da2975824c562c76a4567da9acf

---
+++ includes/Api/ApiAddArticle.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+namespace MediaWiki\Extensions\Collection\Api;
+
+use ApiBase;
+use ApiMain;
+use SpecialCollection;
+use Wikimedia\ParamValidator\ParamValidator;
+
+class ApiAddArticle extends ApiBase {
+	use GetCollectionItemListTrait;
+
+	/**
+	 * @param ApiMain $main main module
+	 * @param string $action name of this module
+	 */
+	public function __construct( ApiMain $main, string $action ) {
+		parent::__construct( $main, $action );
+	}
+
+	/**
+	 * execute the API request
+	 */
+	public function execute() {
+		$params = $this->extractRequestParams();
+		$articleAdded = SpecialCollection::addArticleFromName(
+			$params['namespace'],
+			$params['title'],
+			$params['oldid']
+		);
+
+		if ( !$articleAdded ) {
+			$this->dieWithError( 'coll-api-addarticle-article-not-added' );
+		}
+
+		$this->getCollectionItemListAfterAction();
+	}
+
+	public function getAllowedParams() {
+		return [
+			'namespace' => [
+				ParamValidator::PARAM_TYPE => 'integer',
+				ParamValidator::PARAM_REQUIRED => true,
+			],
+			'title' => [
+				ParamValidator::PARAM_TYPE => 'string',
+				ParamValidator::PARAM_REQUIRED => true,
+			],
+			'oldid' => [
+				ParamValidator::PARAM_TYPE => 'integer',
+				ParamValidator::PARAM_DEFAULT => 0,
+			],
+		];
+	}
+
+	/**
+	 * @return array examples of the use of this API module
+	 */
+	protected function getExamplesMessages(): array {
+		return [
+			'action=collection-addarticle&namespace=0&title=Main_Page&oldid=0'
+				=> 'apihelp-collection-addarticle-example',
+		];
+	}
+}
---
+++ tests/phpunit/includes/api/ApiAddArticleTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Tests for Collection api.php?action=collection-addarticle.
+ *
+ * @group API
+ * @group Database
+ * @group medium
+ *
+ * @covers \MediaWiki\Extensions\Collection\Api\ApiAddArticle
+ */
+class ApiAddArticleTest extends ApiTestCase {
+
+	public function testApiAddArticle_Good() {
+		// Add a namespace to use for collecting articles
+		// Also, set the project namespace to use when testing
+		$this->setMwGlobals( [
+			'wgCommunityCollectionNamespace' => NS_PROJECT,
+			'wgMetaNamespace' => 'Collection',
+		] );
+
+		// Create the page to add to a collection so it's valid.
+		$title = Title::makeTitle( NS_PROJECT, 'UTPage' );
+		$page = $this->getExistingTestPage( $title );
+
+		$apiResultAddArticle = $this->doApiRequest( [
+			'action' => 'collection-addarticle',
+			'namespace' => NS_PROJECT,
+			'title' => $page->getDBkey()
+		] )[0];
+
+		$collectPage = $apiResultAddArticle['collection-addarticle']['collection']['items'][0]['title'];
+		$this->assertSame(
+			'Collection:UTPage',
+			$collectPage,
+			"After the article has been added to the user's collection."
+		);
+
+		$apiResultList = $this->doApiRequest( [
+			'action' => 'collection-list',
+		] )[0];
+
+		$collectPage2 = $apiResultList['collection-list']['items'][0]['title'];
+		$this->assertSame(
+			'Collection:UTPage',
+			$collectPage2,
+			"Retrieve user's collection list and see if article is found."
+		);
+	}
+
+	public function testApiAddArticleBad_Request() {
+		$this->expectException( ApiUsageException::class );
+		$title = $this->getNonexistingTestPage()->getTitle();
+
+		$this->doApiRequestWithToken( [
+			'action' => 'collection-addarticle',
+			'namespace' => $title->getNamespace(),
+			'title' => $title->getDBkey()
+		] )[0];
+	}
+}
