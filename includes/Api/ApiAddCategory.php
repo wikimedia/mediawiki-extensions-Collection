57972d2263fceded40ab505f8032de4e8b56b093
Derick Alangi <alangiderick@gmail.com>
2021-09-22 23:17:58 +0100

    Api: Introduce the `collection-addcategory` API module
    This enable users to add all the articles in a given category to
    their collection
    
    Bug: T291399
    Change-Id: I03f9178a2691417b448bc28388d3c8ab6882db45

---
+++ includes/Api/ApiAddCategory.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+namespace MediaWiki\Extensions\Collection\Api;
+
+use ApiBase;
+use ApiMain;
+use SpecialCollection;
+use Wikimedia\ParamValidator\ParamValidator;
+
+class ApiAddCategory extends ApiBase {
+	use GetCollectionItemListTrait;
+
+	/**
+	 * @param ApiMain $main main module
+	 * @param string $action name of this module
+	 */
+	public function __construct( ApiMain $main, string $action ) {
+		parent::__construct( $main, $action );
+	}
+
+	/**
+	 * execute the API request
+	 */
+	public function execute() {
+		$params = $this->extractRequestParams();
+		 /* $articlesAdded = */ SpecialCollection::addCategoryFromName( $params['title'] );
+
+		// TODO: There is an issue with ::addCategoryFromName that makes successfully
+		// adding articles within a category to the book to return false instead
+		// of true. That is in the case where the number of articles in the category
+		// is not greater than the limit. Limit exceeded will never be true hence
+		// the check below for $articlesAdded will always pass hence needs fixing.
+		/* if ( !$articlesAdded ) {
+			$this->dieWithError( 'coll-api-addcategory-category-does-not-exist' );
+		} */
+
+		$this->getCollectionItemListAfterAction();
+	}
+
+	public function getAllowedParams() {
+		return [
+			// this really should be 'category' instead of 'title'
+			'title' => [
+				ParamValidator::PARAM_TYPE => 'string',
+				ParamValidator::PARAM_DEFAULT => '',
+			],
+		];
+	}
+
+	/**
+	 * @return array examples of the use of this API module
+	 */
+	protected function getExamplesMessages(): array {
+		return [
+			'action=collection-addcategory&title=Main_Page'
+				=> 'apihelp-collection-addcategory-example',
+		];
+	}
+}
---
+++ tests/phpunit/includes/api/ApiAddCategoryTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Tests for Collection api.php?action=collection-addcategory
+ *
+ * @group API
+ * @group Database
+ * @group medium
+ *
+ * @covers \MediaWiki\Extensions\Collection\Api\ApiAddCategory
+ */
+class ApiAddCategoryTest extends ApiTestCase {
+
+	public function testApiAddCategory() {
+		// Create a category first e.g. [[Category:TestCat]]
+		$category = $this->insertPage( 'TestCat', 'pages', NS_CATEGORY );
+		$catFullText = '[[' . $category['title']->getFullText() . ']]';
+
+		$this->assertSame( '[[Category:TestCat]]', $catFullText );
+
+		// Add titles/articles to the category created above
+		$this->insertPage( 'Article1', $catFullText, NS_MAIN );
+		$this->insertPage( 'Article2', $catFullText, NS_PROJECT );
+		$this->insertPage( 'Article3', $catFullText, NS_USER );
+		$this->insertPage( 'Article4', $catFullText, NS_USER_TALK );
+
+		// Add category to the user's collection
+		$apiResult = $this->doApiRequest( [
+			'action' => 'collection-addcategory',
+			'title' => $category['title']->getDBKey()
+		] )[0];
+
+		// Assert that articles from the category are in the
+		// user's collection as expected.
+		$collection = $apiResult['collection-addcategory']['collection']['items'];
+		$this->assertCount( 4, $collection );
+	}
+}
